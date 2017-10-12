8cedbc9fd8d2a7ae2406009b61804abdb36de1f6
sophia.nesamoney <sophia.nesamoney@gmail.com>
2017-10-12 11:07:44 -0700

    First Commit

---
+++ SegueExample/HelloViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HelloViewController.swift
+//  SegueExample
+//
+//  Created by Sophia Nesamoney on 10/10/17.
+//  Copyright © 2017 Sophia Nesamoney. All rights reserved.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+    @IBOutlet weak var greetingLabel: UILabel!
+    var name = "Person"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.text = "Hello \(name)!"
+        
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        greetingLabel.text = "Hello \(name)"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func appendLastName(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
